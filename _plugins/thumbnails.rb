d2eff70f9e60763bdc8c6663e0264ace98a2dcd6
Dan Foster <dan@zem.org.uk>
2014-09-13 16:29:05 +0100

    Index of all photo posts

--- _plugins/thumbnails.rb
+++ _plugins/thumbnails.rb
@@ -2,11 +2,11 @@ require 'nokogiri' # gem install nokogiri
 
 module Jekyll
   module ImageFilter
-    def extract_img(input)
+    def extract_img(input,limit=10)
       doc = Nokogiri::HTML( input )
       output = ""
       img_srcs = doc.css('img')
-      img_srcs[0..5].each do |img|
+      img_srcs[0..limit].each do |img|
           img['class'] = "sidebar_thumbnail"
           output = output + "#{img.to_s}"
       end
